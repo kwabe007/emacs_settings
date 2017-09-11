005864ba06177b92440412bf8dcc82fd4090d3f6
Kwabena Asante-Poku <kwabena.a92@gmail.com>
2017-09-11 17:58:21 +0200

    Updated init

--- init.el
+++ init.el
@@ -1,42 +1,36 @@
-
-(setq load-path (cons "/opt/sicstus/4.2.1/lib/sicstus-4.2.1/emacs/" load-path))
-(autoload 'run-prolog "prolog" "Start a Prolog sub-process." t)
-(autoload 'prolog-mode "prolog" "Major mode for editing Prolog programs." t)
-(setq prolog-use-sicstus-sd t)
-(setq auto-mode-alist (cons '("\\.pl$" . prolog-mode) auto-mode-alist))
+(setq inhibit-startup-message t)
 
 
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
 
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (add-to-list
-   'package-archives
-   '("melpa" . "http://melpa.org/packages/")
-   t)
-  (package-initialize))
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+	(package-refresh-contents)
+	(package-install 'use-package))
+
 
 ;;set register i to direct to this ini-file (.emacs)
-(set-register ?i (cons 'file "~/.emacs"))
+(set-register ?i (cons 'file "~/.emacs.d/init.el"))
 
-;;different language editing modes--------------------------------
-;;python
-(elpy-enable)
-(setq elpy-rpc-backend "jedi")
-(eval-after-load 'company
-  '(add-to-list 'company-backends 'company-irony))
+;;Custom function definitions ------------------------------------
 
-;;c++
-(add-hook 'c++-mode-hook 'irony-mode)
+(defun quick-copy-line ()
+      "Copy the whole line that point is on and move to the beginning of the next line.
+    Consecutive calls to this command append each line to the
+    kill-ring."
+      (interactive)
+      (let ((beg (line-beginning-position 1))
+            (end (line-beginning-position 2)))
+        (if (eq last-command 'quick-copy-line)
+            (kill-append (buffer-substring beg end) (< end beg))
+          (kill-new (buffer-substring beg end))))
+      (beginning-of-line 2))
 
-;;----------------------------------------------------------------
-
-
-
-;;enable windmove for easier window navigation (allows the use of direction arrows)
-(global-set-key (kbd "C-S-<left>") 'windmove-left)
-(global-set-key (kbd "C-S-<right>") 'windmove-right)
-(global-set-key (kbd "C-S-<up>")    'windmove-up)
-(global-set-key (kbd "C-S-<down>")  'windmove-down)
 
 ;;Move current line up/down:
 (defun move-line-up ()
@@ -54,9 +48,29 @@
   (forward-line -1)
   (indent-according-to-mode))
 
+
+;;----------------------------------------------------------------
+
+;;avy bindings
+(global-set-key (kbd "M-g w") 'avy-goto-word-1)
+(global-set-key (kbd "M-g g") 'avy-goto-line)
+
+;;quick-copy line
+(global-set-key (kbd "M-q") 'quick-copy-line)
+
+;;KILL-word
+(global-set-key (kbd "M-<delete>") 'kill-word)
+
+;;enable windmove for easier window navigation (allows the use of direction arrows)
+(global-set-key (kbd "C-S-<left>") 'windmove-left)
+(global-set-key (kbd "C-S-<right>") 'windmove-right)
+(global-set-key (kbd "C-S-<up>")    'windmove-up)
+(global-set-key (kbd "C-S-<down>")  'windmove-down)
+
+;;move line up/down
 (global-set-key (kbd "M-<up>")  'move-line-up)
 (global-set-key (kbd "M-<down>")  'move-line-down)
-
+}
 
 ;;keep the buffers saved when session is closed and a new session is started
 ;;(desktop-save-mode)
@@ -68,5 +82,21 @@
 
 
 ;;enable clipboard copy
-(x-select-enable-clipboard t)
+(setq x-select-enable-clipboard t)
 (setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
+ '(custom-enabled-themes (quote (wheatgrass))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
