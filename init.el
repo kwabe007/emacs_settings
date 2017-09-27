fc50c86723475dec2a6b9c49893cc239128687d9
Kwabena <Kwabena Asante-Poku>
2017-09-27 13:22:33 +0200

    added setting to change auto save file location to tmp directory and adjusted comments with whitespace

--- init.el
+++ init.el
@@ -14,10 +14,10 @@
 	(package-install 'use-package))
 
 
-;;set register i to direct to this ini-file (.emacs)
+;; set register i to direct to this ini-file (.emacs)
 (set-register ?i (cons 'file "~/.emacs.d/init.el"))
 
-;;Custom function definitions ------------------------------------
+;; Custom function definitions ------------------------------------
 
 (defun quick-copy-line ()
       "Copy the whole line that point is on and move to the beginning of the next line.
@@ -32,7 +32,7 @@
       (beginning-of-line 2))
 
 
-;;Move current line up/down:
+;; Move current line up/down:
 (defun move-line-up ()
   "Move up the current line."
   (interactive)
@@ -49,39 +49,36 @@
   (indent-according-to-mode))
 
 
-;;----------------------------------------------------------------
+;; Custom key bindings --------------------------------------------
 
-;;avy bindings
+;; avy bindings
 (global-set-key (kbd "M-g w") 'avy-goto-word-1)
 (global-set-key (kbd "M-g g") 'avy-goto-line)
 
-;;quick-copy line
+;; quick-copy line
 (global-set-key (kbd "M-q") 'quick-copy-line)
 
-;;KILL-word
+;; KILL-word
 (global-set-key (kbd "M-<delete>") 'kill-word)
 
-;;enable windmove for easier window navigation (allows the use of direction arrows)
+;; enable windmove for easier window navigation (allows the use of direction arrows)
 (global-set-key (kbd "C-S-<left>") 'windmove-left)
 (global-set-key (kbd "C-S-<right>") 'windmove-right)
 (global-set-key (kbd "C-S-<up>")    'windmove-up)
 (global-set-key (kbd "C-S-<down>")  'windmove-down)
 
-;;move line up/down
+;; move line up/down
 (global-set-key (kbd "M-<up>")  'move-line-up)
 (global-set-key (kbd "M-<down>")  'move-line-down)
 
+;; Other settings -------------------------------------------------
 
-;;keep the buffers saved when session is closed and a new session is started
-;;(desktop-save-mode)
-
-
-;;hide passwords
+;; hide passwords
 (add-hook 'comint-output-filter-functions
 	  'comint-watch-for-password-prompt)
 
 
-;;enable clipboard copy
+;; enable clipboard copy
 (setq x-select-enable-clipboard t)
 (setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
 (custom-set-variables
@@ -95,8 +92,10 @@
    ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
  '(custom-enabled-themes (quote (wheatgrass))))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  )
+
+;; store all backup and autosave files in the tmp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
